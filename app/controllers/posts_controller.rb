71fbfb6fb3a5a12c061093682c0b2f746a4e3568
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-13 19:51:29 +0900

    update posts#create: now can save post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
+  before_action :set_new_post, only: [:new, :create]
+
   def index
   end
 
   def new
-    @post = Post.new
   end
 
   def create
+    post = Post.create(post_params)
+    if post.errors.empty?
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_new_post
+    @post = Post.new
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
