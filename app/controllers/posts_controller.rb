81cd2ad84bb15a725ee5557bd6f4b83ca445930c
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-13 21:42:56 +0900

    order post by date in desc order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_new_post, only: [:new, :create]
 
   def index
-    @posts = Post.all.includes(:user)
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def new
