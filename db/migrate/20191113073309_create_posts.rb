526106c9adf5794366a78936d932b481536ca9e1
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-13 16:43:31 +0900

    set columns for posts table

--- db/migrate/20191113073309_create_posts.rb
+++ db/migrate/20191113073309_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :content
+      t.string :image
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
