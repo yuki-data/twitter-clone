5f2dd21cc1f383bd80c73bc35b9fe699b3d3edde
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-14 12:27:17 +0900

    configure minimagic: resizing and extension whitelist

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -21,6 +21,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [200, 200]
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
@@ -35,9 +38,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
