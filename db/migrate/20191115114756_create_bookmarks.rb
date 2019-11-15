3cfe8e2a92e6156303871116218251df37de5c6e
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-15 20:54:41 +0900

    set table columns for bookmark model and migrate

--- db/migrate/20191115114756_create_bookmarks.rb
+++ db/migrate/20191115114756_create_bookmarks.rb
@@ -1,7 +1,8 @@
 class CreateBookmarks < ActiveRecord::Migration[5.2]
   def change
     create_table :bookmarks do |t|
-
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
       t.timestamps
     end
   end
