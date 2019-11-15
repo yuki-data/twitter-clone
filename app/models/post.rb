8e52bab9dde7812602c394bc78144be40771c23d
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-15 20:57:55 +0900

    set association between models(post, user, bookmark)

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   mount_uploader :image, PictureUploader
   validates :content, presence: true, unless: :image?
   belongs_to :user
+  has_many :bookmarks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   has_many :posts
+  has_many :bookmarks
 end
