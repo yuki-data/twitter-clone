1c2e90d05f4f66db2988c5b8483b33ee892f4399
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-16 19:37:01 +0900

    create bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,12 @@
 class BookmarksController < ApplicationController
   def create
+    bookmark = Bookmark.create(post_id: params[:post_id], user_id: current_user.id)
+    if bookmark.errors.empty?
+      flash[:notice] = "ブックマークしました"
+    else
+      flash[:alert] = "ブックマークに失敗しました"
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
