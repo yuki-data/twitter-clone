6a9a7aeb83cbb1a15cbba35cf3b4d67db9266dfc
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-17 18:29:05 +0900

    add bookmarks count to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_114756) do
+ActiveRecord::Schema.define(version: 2019_11_17_092617) do
 
   create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_114756) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bookmarks_count", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
