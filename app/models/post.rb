9713889984c3a314536a7124b965d584861eb4fc
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-17 17:35:29 +0900

    add is_bookmarked method to Post to chech the post is bookmarked

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
   has_many :favusers, through: :bookmarks, source: :user
+
+  def is_bookmarked(user_id)
+    favuser_ids.include?(user_id)
+  end
 end
