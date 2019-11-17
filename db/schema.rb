e33ed083af6a3df8e7d517007f13acaa70c0482f
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-17 10:54:38 +0900

    db:rollback to add multiple key index to bookmark to prohibit duplicate bookmark

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_114756) do
-
-  create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_bookmarks_on_post_id"
-    t.index ["user_id"], name: "index_bookmarks_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2019_11_13_073309) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -44,7 +35,5 @@ ActiveRecord::Schema.define(version: 2019_11_15_114756) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookmarks", "posts"
-  add_foreign_key "bookmarks", "users"
   add_foreign_key "posts", "users"
 end
