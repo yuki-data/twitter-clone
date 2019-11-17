0fc58645cd0a43bdf5932e27c587c9583eccc469
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-18 01:04:17 +0900

    add routing to bookmarks#index nested under users for user bookmarks page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :bookmarks, only: [:index]
+  end
   resources :posts, only: [:new, :create, :destroy, :edit, :update] do
     resources :bookmarks, only: [:create, :destroy]
   end
