735ca13b67c4008268eabb5aff557b8aca91bd84
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-17 11:10:23 +0900

    add multiple key index to bookmarks

--- db/migrate/20191115114756_create_bookmarks.rb
+++ db/migrate/20191115114756_create_bookmarks.rb
@@ -4,6 +4,8 @@ class CreateBookmarks < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
       t.timestamps
+
+      t.index [:user_id, :post_id], unique: true
     end
   end
 end
