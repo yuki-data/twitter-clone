bf98cbce02acd4460ed3d861db06f04f7668b52e
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-18 17:49:25 +0900

    add routing to relationships#destroy and create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "posts#index"
   resources :users, only: [:show] do
     resources :bookmarks, only: [:index]
+    resources :relationships, only: [:create, :destroy]
   end
   resources :posts, only: [:new, :create, :destroy, :edit, :update] do
     resources :bookmarks, only: [:create, :destroy]
