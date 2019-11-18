e22555ac5e9742767bbc3a51c6da0cda1f024c0e
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-18 18:55:36 +0900

    add relationships#create and destroy

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,26 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    return if Relationship.find_by(relationship_params)
+    relationship = Relationship.create(relationship_params)
+    if relationship.errors.empty?
+      flash[:notice] = "フォローしました"
+    else
+      flash[:alert] = "フォローに失敗しました"
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    relationship = current_user.relationships.find_by(user_id: params[:user_id])
+    if relationship
+      relationship.destroy
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def relationship_params
+    params.permit(:user_id).merge(fan_id: current_user.id)
+  end
 end
