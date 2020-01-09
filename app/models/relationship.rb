5eea513849f5487c165315780eb5fced890575a1
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-09 19:25:49 +0900

    add validation: prevent following oneself

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,10 @@
 class Relationship < ApplicationRecord
   validates :user_id, uniqueness: { scope: :fan_id }
+  validate :prevent_follow_myself
   belongs_to :user
   belongs_to :fan, class_name: "User"
+
+  def prevent_follow_myself
+    errors.add(:user, "自分自身のフォローはできません") if user_id == fan_id
+  end
 end
