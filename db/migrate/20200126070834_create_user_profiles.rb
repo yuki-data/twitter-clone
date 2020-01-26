c405f31e48df5af12a4dc4be81130d2dc083d3cd
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-26 19:48:06 +0900

    add uniqueness constraint to user_id of userprofile

--- db/migrate/20200126070834_create_user_profiles.rb
+++ db/migrate/20200126070834_create_user_profiles.rb
@@ -1,7 +1,7 @@
 class CreateUserProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :user_profiles do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, unique: true
       t.text :profile
       t.string :thumbnail
       t.string :bg_image
