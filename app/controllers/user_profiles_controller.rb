03922a4647bfda26cbf1e5c39c27ab7ff1fc8670
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-26 19:02:17 +0900

    set model for user_profiles#edit

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,5 +1,9 @@
 class UserProfilesController < ApplicationController
   def edit
-    @user_profile = UserProfile.new
+    @user_profile = UserProfile.find_by(user_id: current_user.id)
+  end
+
+  def update
+    binding.pry
   end
 end
