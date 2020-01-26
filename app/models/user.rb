8a55fe69a8543cfd7f91aa7a45070b0819fd1faf
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-26 23:02:53 +0900

    remove uniqueness constraint of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   has_many :posts, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
