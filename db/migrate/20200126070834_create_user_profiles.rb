e65fdd7139f36846c130945f49ed395bb7637ef5
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-26 16:08:52 +0900

    generate user profile model
    rails g model user_profile user:reference profile:text thumbnail:string bg_image:string

---
+++ db/migrate/20200126070834_create_user_profiles.rb
@@ -0,0 +1,12 @@
+class CreateUserProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_profiles do |t|
+      t.references :user, foreign_key: true
+      t.text :profile
+      t.string :thumbnail
+      t.string :bg_image
+
+      t.timestamps
+    end
+  end
+end
