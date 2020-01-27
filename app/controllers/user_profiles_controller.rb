16456f42b74ea56d7aac096c721dae17ef1ac61f
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-27 16:59:06 +0900

    add update action to user_profiles (temporary)

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,9 +1,33 @@
 class UserProfilesController < ApplicationController
+  before_action :set_user_profile, only: [:edit, :update]
   def edit
-    @user_profile = UserProfile.find_by(user_id: current_user.id)
   end
 
   def update
-    binding.pry
+    respond_to do |format|
+      if @user_profile.update(user_profile_params)
+        format.html {redirect_to edit_user_profiles_path, notice: "プロフィールを更新しました"}
+        # format.js { @status = "success"}
+      else
+        format.html do
+          flash.now[:alert] = "プロフィールの更新に失敗しました"
+          render :edit
+        end
+        format.js do
+          # @status = "fail"
+          # @error_message = @post.errors.full_messages.join("\n")
+        end
+      end
+    end
+  end
+
+  private
+
+  def set_user_profile
+    @user_profile = UserProfile.find_by(user_id: current_user.id)
+  end
+
+  def user_profile_params
+    params.require(:user_profile).permit(:profile, :thumbnail, :bg_image)
   end
 end
